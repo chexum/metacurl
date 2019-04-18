741e5324488a53717c776e36e058de6e823af333
J Farkas <chexum+git@gmail.com>
2019-04-18 20:13:38 +0100

    tests: choose system python
    (also added comments)

--- tests/alltest.sh
+++ tests/alltest.sh
@@ -8,10 +8,40 @@ allgood=0
 unset FLDB
 unset FLMETADIR
 
+export FLMETADIR=$(pwd)/.fl
+mkdir -p $FLMETADIR
+rm -f $FLMETADIR/2018-*
+export FLDB=$(pwd)/.fl/meta.sdb
+rm -f $FLDB
+cat <<EOF|sqlite3 $FLDB
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "meta" (
+path varchar(120),
+fn varchar(80),                 --file
+size integer,                   --size
+stamp time,                     --stamp
+md5 varchar(16),                --dig
+sha1 varchar(20),
+sha256 varchar(32),
+githash varchar(20),
+sig text,
+mimetype varchar(20),           --type
+mimecs varchar(20),
+mimeenc varchar(20),            --encoding
+url varchar(120),               --url
+stored time,                    --present
+server varchar(80),
+etag varchar(80));
+COMMIT;
+EOF
+
 rm -f .meta 030_*.html dup.030_*.html 032_*.html dup.032_*.html
 
 export PYTHONIOENCODING=utf8
+export PYTHON=$(PATH=/bin:/usr/bin:/usr/local/bin:$PATH which python)
 
+#or i in "$@"; do
 for i in [0-9]*txt; do
   b=${i%.txt}
   fnm=$b.out
@@ -19,7 +49,7 @@ for i in [0-9]*txt; do
   tm1=$b.tm1
   tm2=$b.tmp
   rm -f ./$fnm ./.dl_*
-  python ../metacurl --debug-summary --curl-command ./localfetch.sh $fnm 2>./$tm1
+  $PYTHON ../metacurl --debug-summary --curl-command ./localfetch.sh $fnm 2>./$tm1
   sed -E 's/(.stored.):[0-9]+/\1:null/g' <./$tm1 >./$tm2
   rm -f ./$tm1
   diff -u $exp $tm2 >$fnm
